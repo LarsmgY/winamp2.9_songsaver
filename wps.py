9816b7404a987f89e490c1a62c7865aa2a7b9aff
LarsmgY <lars_ystrom@hotmail.com>
2025-08-08 13:50:32 +0200

    Add files via upload

---
+++ wps.py
@@ -0,0 +1,38 @@
+import time
+import pyperclip
+import pyautogui as pg
+import shutil
+import os
+import je_open_cv
+
+
+#bruk cmd og installer pip, installer packages sånn: pip install "je_open_cv" - pip install "pillow"
+#time.sleep(3)
+
+h = None
+while h is None:
+    h = pg.locateCenterOnScreen('D:/wps/winamp.png', confidence=0.9)
+
+pg.click((h), clicks=1, interval=1, button='left')
+
+pg.hotkey('Alt', '3')
+
+r = None
+while r is None:
+    r = pg.locateCenterOnScreen('D:/wps/ID3-window.png', confidence=0.9)
+
+pg.moveTo(r)
+pg.moveRel(-264, 58, duration=.1)
+pg.dragRel(2500, 0, duration=.6) 
+
+time.sleep(0.1)
+
+pg.hotkey('Ctrl', 'c')
+pg.hotkey('Esc')
+
+s = pyperclip.paste()
+print(s)
+
+shutil.copy((s), 'D:\Musikk\python copy script')
+
+pg.click((h), clicks=1, interval=1, button='left') 
